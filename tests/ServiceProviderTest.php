5d7b451f43fe549ed47ee2358f91e84b08b58c0e
Abdelrahman Omran <me@omranic.com>
2016-12-20 15:46:19 +0200

    Rewrite tests in a more efficient and low footprint way

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -17,23 +17,45 @@ declare(strict_types=1);
 
 namespace Rinvex\Authy\Test;
 
+use ReflectionClass;
+use PHPUnit_Framework_TestCase;
+use Illuminate\Container\Container;
+use Illuminate\Support\ServiceProvider;
 use Rinvex\Authy\Providers\AuthyServiceProvider;
-use GrahamCampbell\TestBench\AbstractPackageTestCase;
-use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
 
-class ServiceProviderTest extends AbstractPackageTestCase
+class ServiceProviderTest extends PHPUnit_Framework_TestCase
 {
-    use ServiceProviderTrait;
-
-    /**
-     * Get the service provider class.
-     *
-     * @param \Illuminate\Contracts\Foundation\Application $app
-     *
-     * @return string
-     */
-    protected function getServiceProviderClass($app): string
+    /** Get the service provider class. */
+    protected function getServiceProviderClass(): string
     {
         return AuthyServiceProvider::class;
+    }
+
+    /** @test */
+    public function it_is_a_service_provider()
+    {
+        $class = $this->getServiceProviderClass();
+
+        $reflection = new ReflectionClass($class);
+
+        $provider = new ReflectionClass(ServiceProvider::class);
+
+        $msg = "Expected class '$class' to be a service provider.";
+
+        $this->assertTrue($reflection->isSubclassOf($provider), $msg);
+    }
+
+    /** @test */
+    public function it_has_provides_method()
+    {
+        $class = $this->getServiceProviderClass();
+        $reflection = new ReflectionClass($class);
+
+        $method = $reflection->getMethod('provides');
+        $method->setAccessible(true);
+
+        $msg = "Expected class '$class' to provide a valid list of services.";
+
+        $this->assertInternalType('array', $method->invoke(new $class(new Container())), $msg);
     }
 }
